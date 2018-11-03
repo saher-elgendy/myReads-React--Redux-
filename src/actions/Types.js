0dc9b9ea10c7cfcf037d6480fb3cf518a657b6a8
unknown <saherelgendy1990@gmail.com>
2018-11-03 07:03:25 +0200

    removing unwanted actiosn file

--- src/actions/Types.js
+++
@@ -1,1 +0,0 @@
-export const FETCH_BOOKS = 'FETCH_BOOKS';
--- src/actions/postsAction.js
+++
@@ -1,9 +0,0 @@
-import { FETCH_BOOKS } from './Types'
-import BooksAPI from '../BooksAPI'
-
-const fetchBooks = ()=> dispatch => {
-  BooksAPI.getAll().then(res => res.json()).then(books => dispatch({
-  	type: FETCH_BOOKS,
-  	payload: books
-  }));
-}
