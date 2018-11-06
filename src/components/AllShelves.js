c574c58f1118aa32a898db8b4f60654a0b571572
unknown <saherelgendy1990@gmail.com>
2018-11-06 06:15:32 +0200

    removing unused variables

--- src/components/AllShelves.js
+++ src/components/AllShelves.js
@@ -1,4 +1,4 @@
-  import React, {Component} from 'react';
+  import React from 'react';
   import BookShelf from './BookShelf';
   import { Link } from 'react-router-dom';
 
--- src/components/Book.js
+++ src/components/Book.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ShelfChanger from './shelfChanger'
 import {connect } from 'react-redux';
 
